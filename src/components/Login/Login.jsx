56ebdd6d7b4d78e8ce54c6da9553157ba5ad3bf0
akashyadav2000 <125122328+akashyadav2000@users.noreply.github.com>
2024-05-29 20:40:19 +0530

    fix 31

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -65,7 +65,7 @@ function Login() {
           setFeedbackMessage("Login successful ...");
           setTimeout(() => {
             navigate("/");
-          }, 200000000);
+          }, 2000);
         } else {
           setFeedbackMessage(response.data.message || "Login failed. Please try again.");
         }
